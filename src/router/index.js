9b09e7487d5c3a7889eec09ec70b5e3318b8d0c9
lyllovelemon <732193462@qq.com>
2019-04-03 17:18:28 +0800

    remove helloWorld

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import dialog from '../components/dialog/index';
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },{
-      path:'/dialog',
+      path:'/',
       name:'dialog',
       component:dialog
     }
