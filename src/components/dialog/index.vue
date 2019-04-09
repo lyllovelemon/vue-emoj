73c1a308edb919a1c63d0106415b24fd09026027
lyllovelemon <732193462@qq.com>
2019-04-09 17:56:33 +0800

    change style and vue

--- src/components/dialog/index.vue
+++ src/components/dialog/index.vue
@@ -1,97 +1,81 @@
 <template>
-  <div class="main">
-    <div class="header">
-      <div class="search-bar">
-        <el-input
-          placeholder="搜索"
-          suffix-icon="el-icon-search"
-          v-model="input">
-        </el-input>
-      </div>
-      <p class="name">微信昵称</p>
-    </div>
-    <div class="count-view">
-      <div class="panel">
-        <div class="chat-item" @click="getContact(index)" :class="chatId===index?'active':''"
-             v-for="(item,index) in chatInfo" :key="index">
-          <div class="avatar">
-            <img src="./avatar.jpg" alt="">
-          </div>
-          <div class="info">
-            <p class="nickname">{{item.name}}</p>
-            <p class="content">{{item.msg}}</p>
-          </div>
-          <div class="ext">{{item.num}}</div>
+  <div class="list">
+    <div class="main">
+      <div class="header">
+        <div class="search-bar">
+          <el-input placeholder="搜索" @change="blurSearch" suffix-icon="el-icon-search" v-model="searchText"></el-input>
         </div>
+        <p class="name">粽子</p>
       </div>
-      <div class="chat-area">
-        <div class="scroll-wrapper">
-          <div class="chat-content">
-            <div class="other-message">
-              <div class="date">{{date}}</div>
-              <div class="content" v-if="otherMsg.length">
-                <div class="other" v-for="item in otherMsg">
-                  {{item}}
+      <div class="count-view">
+        <!--消息列表-->
+        <div class="panel">
+          <div class="chat-item" @click="getContact(index)" :class="chatId===index?'active':''"
+               v-for="(item,index) in chatInfo" :key="index">
+            <div class="avatar">
+              <img src="./avatar.jpg" alt="">
+            </div>
+            <div class="info">
+              <p class="nickname">{{item.name}}</p>
+              <p class="content">{{item.msg}}</p>
+            </div>
+            <div class="ext" v-if="item.num>0">{{item.num}}</div>
+          </div>
+        </div>
+        <div class="chat-area">
+          <div class="scroll-wrapper">
+            <!--聊天内容-->
+            <div class="chat-content">
+              <div class="other-message" v-if="otherMsg[chatId]">
+                <div class="date">{{otherMsg[chatId].date}}</div>
+                <div class="content" >
+                  <div class="other" >
+                    {{otherMsg[chatId].msg}}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="message" v-if="senMsg.length">
-              <div class="date">{{sendDate}}</div>
-              <div class="content">
-                <div class="mine" v-for="item in senMsg">
-                  <span v-if="sendImg && !item" v-html="sendImg"></span>
-                  <span v-if="item && !sendImg">{{item}}</span>
-                  <span v-if="item && sendImg">表情包拼文字，OMG</span>
-                </div>
-              </div>
+              <div class="message" v-if="sendMsg[chatId]">
+                <template>
+                  <div class="date">{{sendMsg[chatId].date[0]}}</div>
+                  <div class="content">
+                    <div class="mine" v-if="!!sendMsg[chatId].msg" v-for="item in sendMsg[chatId].msg">
+                      <span  v-html="item"></span>
+                    </div>
+                  </div>
+                </template>
 
-            </div>
-
-          </div>
-
-        </div>
-        <div class="box-footer">
-          <div class="emoj-panel" v-if="showEmoj">
-            <div class="expression">
-              <ul class="top">
-                <li v-for="item in emojType" :class="isShow?'active':''">{{item.name}}</li>
-              </ul>
-              <div class="emoj-wrapper">
-                <emotion @emotion="handleEmotion" class="emoj-list"></emotion>
               </div>
             </div>
-
           </div>
-          <div class="toolbar">
-            <span class="icon-wrapper" @click="openEmoj">
-               <svg class="icon" aria-hidden="true">
-              <use xlink:href="#icon-biaoqing"></use>
-              </svg>
-            </span>
-            <span class="icon-wrapper">
-              <svg class="icon" aria-hidden="true">
-              <use xlink:href="#icon-jieping"></use>
-            </svg>
-           </span>
-            <span class="icon-wrapper file-wrapper" @click="openFile">
-             <input type="file" class="file-select">
-             <label class="svg-file">
-               <svg class="icon" aria-hidden="true">
-              <use xlink:href="#icon-wenjian"></use>
-            </svg>
-             </label>
-
-           </span>
-          </div>
-          <div class="content" @click="closeEmoj">
-            <!--表情包文字转义为图片-->
-            <span class="img-wrapper" v-html="content.replace(/\#[\u4E00-\u9FA5]{1,3}\;/gi, emotion)"></span>
-            <textarea name="" id="" cols="10" rows="250" v-model="txt">
-           </textarea>
-          </div>
-          <div class="action">
-            <span class="desc">按下Ctrl+Enter换行</span>
-            <a @click="sendMsg" class="btn send-btn">发送</a>
+          <div class="box-footer">
+            <div class="expression" v-if="showExpression">
+              <span v-for="item in expression" @click="appendToText(item)">{{item}}</span>
+              <a class="add-new" @click="openAdd">新增</a>
+            </div>
+            <el-dialog
+              title="新增常用语"
+              :visible.sync="dialogVisible"
+              width="30%"
+              :before-close="handleClose">
+              <textarea v-model="wordValue" class="my-word" contenteditable ref="myWord"></textarea>
+              <span slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="addExpression">添加</el-button>
+              </span>
+            </el-dialog>
+            <div class="toolbar">
+              <!--表情入口-->
+              <emotion-panel  @picked="onPickedEmoji" :positionIsUp="true"></emotion-panel>
+              <span class="icon-wrapper" @click="onPickedExpression">
+                常用语
+              </span>
+            </div>
+            <div class="content">
+              <!---消息编辑-->
+              <emotion-editor ref="emotionEditor"></emotion-editor>
+            </div>
+            <div class="action">
+              <a @click="handleSendMsg" class="btn send-btn">发送</a>
+            </div>
           </div>
         </div>
       </div>
@@ -99,6 +83,7 @@
   </div>
 </template>
 <script>
+  import './index.less'
   import '../../assets/iconfont';
   import emotion from '../emotion/index';
 
@@ -325,299 +310,4 @@
     }
   };
 </script>
-<style lang="less">
-  .main {
-    height: 100%;
-    max-width: 1000px;
-    min-width: 800px;
-    margin: 0 auto;
-    padding-bottom: 40px;
-    border: 1px solid #eeeeee;
-    .icon {
-      width: 15px;
-      height: 15px;
-      vertical-align: -0.15em;
-      fill: currentColor;
-      overflow: hidden;
-      cursor: pointer;
-    }
-
-    .header {
-      position: relative;
-      width: 100%;
-      height: 76px;
-      border-bottom: 1px solid #dddddd;
-      display: flex;
-      align-items: center;
-
-      .search-bar {
-        width: 150px;
-        margin-left: 10px;
-      }
-
-      .name {
-        padding-left: 140px;
-      }
-    }
-
-    .count-view {
-      display: flex;
-      justify-content: flex-start;
-
-      .panel {
-        display: flex;
-        flex-direction: column;
-        width: 280px;
-        height: 350px;
-        overflow-y: scroll;
-
-        .chat-item {
-          padding: 12px 18px 11px;
-          display: flex;
-          align-items: center;
-          position: relative;
-          border-bottom: 1px solid #dddddd;
-          cursor: pointer;
-
-          .avatar {
-            margin-right: 10px;
-            img {
-              width: 40px!important;
-              border-radius: 2px;
-            }
-          }
-
-          .ext {
-            position: absolute;
-            right: 10px;
-            bottom: 15px;
-          }
-
-        }
-
-        .active {
-          background: rgba(0, 0, 0, .2);
-        }
-      }
-
-      .panel::-webkit-scrollbar {
-        width: 4px;
-        height: 4px;
-      }
-
-      .panel::-webkit-scrollbar-thumb {
-        border-radius: 5px;
-        box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.1);
-      }
-
-      .chat-area {
-        width: 720px;
-        position: relative;
-        background-color: #eee;
-
-        .scroll-wrapper {
-          width: 100%;
-          bottom: 180px;
-          overflow: hidden;
-          cursor: pointer;
-
-          .chat-content {
-            display: flex;
-            flex-direction: column;
-            height: 220px;
-            overflow-y: scroll;
-
-            .other-message {
-              .date {
-                display: flex;
-                justify-content: center;
-                margin: 10px auto;
-                max-width: 50%;
-                color: #b2b2b2;
-                font-size: 12px;
-              }
-
-              .content {
-                display: flex;
-                flex-direction: column;
-                align-items: flex-start;
-                justify-content: flex-start;
-                margin-left: 10px;
-
-                .other {
-                  background: #FFFFFF;
-                  font-size: 14px;
-                  vertical-align: top;
-                  padding: 5px;
-                  margin: 10px 0;
-                }
-              }
-            }
-
-            .message {
-              .date {
-                display: flex;
-                justify-content: center;
-                margin: 10px auto;
-                max-width: 50%;
-                color: #b2b2b2;
-                font-size: 12px;
-              }
-
-              .content {
-                display: flex;
-                flex-direction: column;
-                align-items: flex-end;
-                justify-content: flex-end;
-                margin-right: 10px;
-
-                .mine {
-                  background: #b2e281;
-                  font-size: 14px;
-                  vertical-align: top;
-                  padding: 5px;
-                  margin: 10px 0;
-                }
-              }
-            }
-          }
-
-          .chat-content::-webkit-scrollbar {
-            width: 7px;
-          }
-
-          .chat-content::-webkit-scrollbar-thumb {
-            border-radius: 10px;
-            background: #6c6e71;
-            border-radius: 7px;
-          }
-        }
-
-        .box-footer {
-          height: 130px;
-          margin-right: 19px;
-          border-top: 1px solid #d6d6d6;
-          position: relative;
-
-          .emoj-panel {
-            width: 500px;
-            height: 174px;
-            background: #FFFFFF;
-            position: absolute;
-            left: 50%;
-            transform: translateX(-68%);
-            z-index: 99;
-            top: -181px;
-
-            .expression::before {
-              content: "";
-              position: absolute;
-              left: 16px;
-              top: 100%;
-              margin-left: -7px;
-              border: 7px solid transparent;
-              border-top-color: #cfcfcf;
-            }
-
-            .expression {
-              .top {
-                padding: 8px 20px 0;
-                list-style: none;
-                display: flex;
-                justify-content: flex-start;
-                background: #f2f2f2;
-
-                li {
-                  list-style: none;
-                  color: #333;
-                  padding: 5px 20px;
-                  font-size: 14px;
-                  cursor: pointer;
-                }
-
-                .active {
-                  background: #FFFFFF;
-                }
-              }
-
-              .emoj-wrapper {
-                .emoj-list {
-                  width: 480px;
-                  height: 134px;
-                  overflow-y: scroll;
-                  margin: 0 20px;
-                }
-              }
-            }
-          }
-        }
-
-        .toolbar {
-          height: 30px;
-          padding: 5px 17px;
-
-          .icon-wrapper {
-            display: inline-flex;
-            margin: 0 5px;
-            width: 15px;
-            height: 15px;
-            align-items: center;
-            justify-content: center;
-          }
-
-          .file-wrapper {
-            position: relative;
-
-            .file-select {
-              position: absolute;
-              clip: rect(1px, 1px, 1px, 1px);
-              left: 0;
-            }
-
-            .svg-file {
-              width: 100%;
-              height: 100%;
-            }
-          }
-        }
-
-        .content {
-          position: relative;
-
-          .img-wrapper {
-            padding-left: 20px;
-            display: inline-flex;
-            flex-wrap: wrap;
-          }
-
-          textarea {
-            width: 100%;
-            height: 58px;
-            overflow-y: auto;
-            overflow-x: hidden;
-            padding-left: 20px;
-            outline: none;
-            background: #EEEEEE;
-            border: 0;
-            font-size: 14px;
-          }
-        }
-
-        .action {
-          text-align: right;
-          position: absolute;
-          bottom: 10px;
-          right: 10px;
-
-          .send-btn {
-            background-color: #fff;
-            color: #222;
-            padding: 5px 30px;
-          }
-        }
-      }
-    }
-  }
-
-</style>
 
